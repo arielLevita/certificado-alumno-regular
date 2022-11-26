20289ca31146bd2d9b6ce569d03c3dfa14364f1c
arielLevita <levita.ariel@gmail.com>
2022-11-25 23:52:22 -0300

    forth commit

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const career = ["Profesorado de Educación Secundaria en Geografía",
                 "Tecnicatura Superior en Tiempo Libre y Recreación",
                 "Tecnicatura Superior en Gestión Administrativa Orientada a la Producción"];
 
-const resolution = ["303/14","544/19","304/14","547/19","309/14","310/14","328/22","308/14","177/16","409/18"];
+const resolution = ["303/14","544/19","304/14","547/19", "302/14", "536/19", "309/14","310/14","328/22","308/14","177/16","409/18"];
 
 let btnGenerar = document.getElementById("btnGenerate");
     
@@ -42,9 +42,9 @@ btnGenerar.addEventListener('click', () => {
         <p>A los ${dia} días del mes de ${mes} de ${anio}, en la ciudad de Esquel, se extiende el presente CERTIFICADO a pedido de el/la interesado/a, para ser presentado ante las autoridades de ${destino}.</p>
         <span id="firmas"></span>
         <div class="text-center lh-1" id="datosInstituto">
-            <p class="m-0">ISFD N° 809 - República de Costa Rica</p>
-            <p class="m-0">Esquel-Chubut / Dirección: Sarmiento 940 / Teléfono (2945) 452323</p>
-            <p class="m-0">Correo electrónico: 809esquel@gmail.com</p>
+            <p class="m-0 pieDePagina">ISFD N° 809 - República de Costa Rica</p>
+            <p class="m-0 pieDePagina">Sarmiento 940 / Esquel-Chubut / (2945) 452323</p>
+            <p class="m-0 pieDePagina">isfd809-chu.infd.edu.ar / 809esquel@gmail.com</p>
         </div>
     </div>`;
 
