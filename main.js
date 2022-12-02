77420273b4cff188004292e70418aa1c1d9a4d8d
arielLevita <levita.ariel@gmail.com>
2022-12-02 00:01:04 -0300

    sixth commit. Eliminado el parentezco

--- main.js
+++ main.js
@@ -18,14 +18,14 @@ const resolution = ["303/14","544/19","304/14","547/19", "302/14", "536/19", "30
 let btnGenerar = document.getElementById("btnGenerate");
     
 btnGenerar.addEventListener('click', () => {
-    let pariente = "";
-    let checkbox = document.getElementById("autoSizingCheck");
-    let nombrePariente = document.getElementById("floatingRelativeName").value;
-    let apellidoPariente = document.getElementById("floatingRelativeLastName").value;
-    let dniPariente = document.getElementById("floatingRelativeId").value;
-    if (checkbox.checked) {
-        pariente = ` hijo/a de ${nombrePariente} ${apellidoPariente} (DNI N°${dniPariente}),`;
-    };
+    // let pariente = "";
+    // let checkbox = document.getElementById("autoSizingCheck");
+    // let nombrePariente = document.getElementById("floatingRelativeName").value;
+    // let apellidoPariente = document.getElementById("floatingRelativeLastName").value;
+    // let dniPariente = document.getElementById("floatingRelativeId").value;
+    // if (checkbox.checked) {
+    //     pariente = ` hijo/a de ${nombrePariente} ${apellidoPariente} (DNI N°${dniPariente}),`;
+    // };
     
     let nombre = document.getElementById("floatingName").value;
     let apellido = document.getElementById("floatingLastName").value;
@@ -35,10 +35,11 @@ btnGenerar.addEventListener('click', () => {
     let plan = resolution[document.getElementById("floatingResolution").value - 1];
     let destino = document.getElementById("floatingDestination").value;
     
+    //! En caso de volver a agregar el parentezco, insertar la variable pariente en el párrafo del certificado.
     document.getElementById("formulario").innerHTML =
     `<div class="container d-flex flex-column justify-content-center certificado">
         <h4 class="mb-5 text-center">Certificado de Alumno Regular</h4>
-        <p>La Dirección del Instituto Superior de Formación Docente Nº 809 CERTIFICA que <span class="text-uppercase fw-bold">${nombre} ${apellido}</span> (DNI Nº${dni}),${pariente} es alumno/a regular en ${anioCursada} año de la carrera de ${carrera}, Res. MECH ${plan}.</p>
+        <p>La Dirección del Instituto Superior de Formación Docente Nº 809 CERTIFICA que <span class="text-uppercase fw-bold">${nombre} ${apellido}</span> (DNI Nº${dni}), es alumno/a regular en ${anioCursada} año de la carrera de ${carrera}, Res. MECH ${plan}.</p>
         <p>A los ${dia} días del mes de ${mes} de ${anio}, en la ciudad de Esquel, se extiende el presente CERTIFICADO a pedido de el/la interesado/a, para ser presentado ante las autoridades de ${destino}.</p>
         <span id="firmas"></span>
         <div class="text-center lh-1" id="datosInstituto">
